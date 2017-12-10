6117670b84248e46af114b4ccf40cf1b30bc62d3
pravosleva <selection4test@gmail.com>
2017-12-10 23:22:16 +0300

    Updates
    - Jets added with random coordinates.

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ let express = require('express');
 let app = express();
 app.set('port', process.env.PORT || 1111);
 var cors = require('cors');
-//Read more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//  see more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use(cors());
 app.get('/jetsArray', (req, res) => {
   res.type('application/json');
+  // see more: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  let _getRandomArbitrary = (min, max) => ((Math.random() * (max - min) + min).toFixed(6));
+  //let _getCoordinates = () => { lat: _getRandomArbitrary(10, 90), lon: _getRandomArbitrary(10, 90) };
   let jetsArray = [
     {
       flightNumber: '001a',
-      coordinates: { lat: 77.777777, lon: 88.888888 }
+      coordinates: { lat: _getRandomArbitrary(10, 90), lon: _getRandomArbitrary(10, 90) }
     },
     {
       flightNumber: '002b',
-      coordinates: { lat: 55.555555, lon: 66.666666 }
+      coordinates: { lat: _getRandomArbitrary(10, 90), lon: _getRandomArbitrary(10, 90) }
+    },
+    {
+      flightNumber: '003c',
+      coordinates: { lat: _getRandomArbitrary(10, 90), lon: _getRandomArbitrary(10, 90) }
     },
   ];
   res.send(jetsArray);
@@ -21,13 +28,14 @@ app.get('/jetsArray', (req, res) => {
 app.use((req, res) => {
   res.type('application/json');
   res.status(404);
-  res.send({'msg': '404 - Not found'});
+  res.send({ msg: '404 - Not found' });
 });
 app.use((req, res) => {
-  res.type({'msg': 'application/json'});
+  res.type({ msg: 'application/json' });
   res.status(500);
-  res.send('500 - Internal Server Error');
+  res.send({ msg: '500 - Internal Server Error' });
 });
+
 app.listen(app.get('port'), function(){
   console.log('Express running on ' + app.get('port') + '; Ctrl+C to end.');
 });
